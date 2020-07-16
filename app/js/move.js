fa1421318e4975d47e608f6b4af8eb50e183c8dd
yankovski88 <boets003@mail.ru>
2020-07-16 21:18:31 +0300

    add ()function

--- app/js/move.js
+++ app/js/move.js
@@ -1,56 +1,56 @@
-var move = document.querySelector(".move");
-var pageBody = document.querySelector(".page-body");
+(function () {
 
 
-move.addEventListener("mousedown", function (evt) {
-    evt.preventDefault();
-    console.log("on push down on me");
-
-    var startCoords = {
-        x: evt.clientX,
-        y: evt.clientY
-    }
+    var moveWindow = function () {
 
 
-    var onMouseMove = function (moveEvt) {
-        moveEvt.preventDefault();
+        var move = document.querySelector(".move");
 
-        var shift = {
-            x: startCoords.x - moveEvt.clientX,
-            y: startCoords.y - moveEvt.clientY
-        }
 
-        startCoords = {
-            x: moveEvt.clientX,
-            y: moveEvt.clientY
-        }
+        move.addEventListener("mousedown", function (evt) {
+            evt.preventDefault();
+            console.log("on push down on me");
 
-        move.style.top = (move.offsetTop - shift.y) + "px";
-        move.style.left = (move.offsetLeft -shift.x) + "px";
+            var startCoords = {
+                x: evt.clientX,
+                y: evt.clientY
+            }
+
+
+            var onMouseMove = function (moveEvt) {
+                moveEvt.preventDefault();
+
+                var shift = {
+                    x: startCoords.x - moveEvt.clientX,
+                    y: startCoords.y - moveEvt.clientY
+                }
+
+                startCoords = {
+                    x: moveEvt.clientX,
+                    y: moveEvt.clientY
+                }
+
+                move.style.top = (move.offsetTop - shift.y) + "px";
+                move.style.left = (move.offsetLeft - shift.x) + "px";
+
+            };
+
+            var onMouseUp = function (upEvt) {
+                upEvt.preventDefault();
+                document.removeEventListener("mousemove", onMouseMove);
+                document.removeEventListener("mouseup", onMouseUp);
+            };
+
+
+            document.addEventListener("mousemove", onMouseMove);
+            document.addEventListener("mouseup", onMouseUp);
+        });
 
 
     };
 
-    var onMouseUp = function (upEvt) {
-        upEvt.preventDefault();
-        document.removeEventListener("mousemove", onMouseMove);
-        document.removeEventListener("mouseup", onMouseUp);
-    };
-
-
-
-
-    document.addEventListener("mousemove", onMouseMove);
-    document.addEventListener("mouseup", onMouseUp);
-});
-
-
-
-
-
-
-
-
+    moveWindow();
+})();
 
 
 // 'use strict';
